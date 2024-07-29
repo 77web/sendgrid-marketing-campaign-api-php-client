4999946624a8ac5d401ed7f4b62ca1359614b637
77web <hishida77web@gmail.com>
2024-07-29 23:09:02 +0900

    include errors in CreateRecipientsResponse

---
+++ src/Recipients/CreateRecipientError.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Linkage\SendgridMarketingCampaignApiClient\Recipients;
+
+readonly class CreateRecipientError
+{
+    /** @param array<int> $errorIndices */
+    public function __construct(
+        public string $message,
+        public array $errorIndices,
+    ) {
+    }
+}
