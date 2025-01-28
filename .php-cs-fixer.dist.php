81345a406a2c3e982b29a931fcd89f263dc3c8cf
77web <hishida77web@gmail.com>
2025-01-28 10:57:06 +0900

    updated php-cs-fixer and phpstan configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,15 +2,27 @@
 
 declare(strict_types=1);
 
-require_once 'vendor/autoload.php';
-
-use Quartetcom\StaticAnalysisKit\PhpCsFixer\Config;
-
 $finder = PhpCsFixer\Finder::create()
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
+    ->exclude(['vendor'])
     ->in(__DIR__)
-    ->exclude('vendor')
 ;
 
-return (new Config())
+$config = new PhpCsFixer\Config();
+$config
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PHPUnit75Migration:risky' => true,
+        '@PSR12' => true,
+        'no_unused_imports' => true,
+        'heredoc_indentation' => false,
+        'use_arrow_functions' => false,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+    ])
+    ->setLineEnding("\n")
     ->setFinder($finder)
 ;
+
+return $config;
